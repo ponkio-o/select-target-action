8794275b320a0012747a95db09e98e89c27ddfef
ponkio-o <29038315+ponkio-o@users.noreply.github.com>
2023-02-15 10:08:41 +0900

    added default target

--- generate-matrix/src/lib.ts
+++ generate-matrix/src/lib.ts
@@ -13,6 +13,7 @@ function getLabels(): string[] {
 export function getWorkDir(configPath: string): string[] {
     const labels = getLabels();
     const configData = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
+    const defaultTargetKey = 'default'
 
     let workDirs = new Array();
     for (var idx in labels) {
@@ -23,7 +24,14 @@ export function getWorkDir(configPath: string): string[] {
     }
 
     if (workDirs.length == 0) {
-        core.setFailed("Labels must be selected.");
+        if (configData[defaultTargetKey] != undefined && configData[defaultTargetKey].length != 0) {
+            core.info("Use default target")
+            workDirs = configData[defaultTargetKey]
+        } else {
+            core.debug(`workDirs: ${String(workDirs)}`)
+            core.debug(`labels: ${String(labels)}`)
+            core.setFailed("Select labels or set default target. See  https://github.com/ponkio-o/select-target-action/blob/main/README.md#default-target");
+        }
     }
 
     // merge working directories
